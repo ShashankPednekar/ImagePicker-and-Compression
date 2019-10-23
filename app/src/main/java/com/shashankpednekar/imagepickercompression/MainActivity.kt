7405bb54408fe9c9b2edf9a7a2d1098a7ee8bf0c
Shashank Pednekar <shashank.pednekar@creditmate.in>
2019-10-23 16:06:39 +0530

    feat: project basic setup complete and required permission asked on button click

--- app/src/main/java/com/shashankpednekar/imagepickercompression/MainActivity.kt
+++ app/src/main/java/com/shashankpednekar/imagepickercompression/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.shashankpednekar.imagepickercompression
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+private const val REQ_CAPTURE = 100
+
+class MainActivity : ParentActivity(R.layout.activity_main) {
+    val permissions = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.CAMERA
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        btn_capture.setOnClickListener {
+            if (isPermissionsAllowed(permissions, true, REQ_CAPTURE)) {
+                chooseImage()
+            }
+        }
+    }
+
+    private fun chooseImage() {
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQ_CAPTURE -> {
+                if (isAllPermissionsGranted(grantResults)) {
+                    chooseImage()
+                } else {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.permission_not_granted),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
     }
 }
