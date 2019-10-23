04f5f117c56659cd933b67c13566d5f5f6c1fd9c
Shashank Pednekar <shashank.pednekar@creditmate.in>
2019-10-23 18:26:40 +0530

    feat: choose image from camera or gallery using intent

--- app/src/main/java/com/shashankpednekar/imagepickercompression/MainActivity.kt
+++ app/src/main/java/com/shashankpednekar/imagepickercompression/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.shashankpednekar.imagepickercompression
 
 import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.os.Parcelable
+import android.provider.MediaStore
 import android.widget.Toast
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.util.ArrayList
 
 private const val REQ_CAPTURE = 100
+private const val RES_IMAGE = 100
 
 class MainActivity : ParentActivity(R.layout.activity_main) {
-    val permissions = arrayOf(
+    private var imgPath: String = ""
+    private var imageUri: Uri? = null
+    private val permissions = arrayOf(
         Manifest.permission.READ_EXTERNAL_STORAGE,
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
         Manifest.permission.CAMERA
@@ -22,10 +35,6 @@ class MainActivity : ParentActivity(R.layout.activity_main) {
                 chooseImage()
             }
         }
-    }
-
-    private fun chooseImage() {
-
     }
 
     override fun onRequestPermissionsResult(
@@ -47,5 +56,89 @@ class MainActivity : ParentActivity(R.layout.activity_main) {
                 }
             }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            RES_IMAGE -> {
+                if (resultCode == Activity.RESULT_OK) {
+
+                }
+            }
+        }
+    }
+
+    private fun chooseImage() {
+        startActivityForResult(getPickImageIntent(), RES_IMAGE)
+    }
+
+    private fun getPickImageIntent(): Intent? {
+        var chooserIntent: Intent? = null
+
+        var intentList: MutableList<Intent> = ArrayList()
+
+        val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, setImageUri())
+        takePhotoIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        takePhotoIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+
+        intentList = addIntentsToList(this, intentList, pickIntent)
+        intentList = addIntentsToList(this, intentList, takePhotoIntent, imageUri)
+
+        if (intentList.size > 0) {
+            chooserIntent = Intent.createChooser(
+                intentList.removeAt(intentList.size - 1),
+                getString(R.string.select_capture_image)
+            )
+            chooserIntent!!.putExtra(
+                Intent.EXTRA_INITIAL_INTENTS,
+                intentList.toTypedArray<Parcelable>()
+            )
+        }
+
+        return chooserIntent
+    }
+
+    private fun setImageUri(): Uri {
+        val folder = File("${getExternalFilesDir(Environment.DIRECTORY_DCIM)}")
+        folder.mkdirs()
+
+        val file = File(folder, "Image_Tmp.png")
+        if (file.exists())
+            file.delete()
+        file.createNewFile()
+        imageUri = FileProvider.getUriForFile(
+            this,
+            BuildConfig.APPLICATION_ID + getString(R.string.file_provider_name),
+            file
+        )
+        imgPath = file.absolutePath
+        return imageUri!!
+    }
+
+
+    private fun addIntentsToList(
+        context: Context,
+        list: MutableList<Intent>,
+        intent: Intent,
+        uri: Uri? = null
+    ): MutableList<Intent> {
+        val resInfo = context.getPackageManager().queryIntentActivities(intent, 0)
+        for (resolveInfo in resInfo) {
+            val packageName = resolveInfo.activityInfo.packageName
+            if (uri != null)
+                context.grantUriPermission(
+                    packageName,
+                    uri,
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                )
+            val targetedIntent = Intent(intent)
+            targetedIntent.setPackage(packageName)
+            list.add(targetedIntent)
+        }
+        return list
     }
 }
